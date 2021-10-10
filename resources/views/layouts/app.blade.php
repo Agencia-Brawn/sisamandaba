506edf178e1d60df1694934bb8d2beec496ce345
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2021-10-10 01:59:58 -0300

    ajuste da logo menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,7 @@
         <nav style="z-index: 1000;" class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container" >
                 <a class="navbar-brand" href="{{ url('/') }}" style="font-size: 15px; font-weigth: 500;">
-                    <img src="img/logo-menu.png" style="margin-left: -70px;" alt="Sistema Amandaba" class="img-fluid logo-menu" >
+                    <img src="img/logo-menu.png" style="max-width: 175px;" alt="Sistema Amandaba" class="logo-menu" >
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <!-- <span class="navbar-toggler-icon"></span> -->
